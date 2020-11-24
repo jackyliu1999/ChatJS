2eba584cf3592c7de6e2448d8e197ca39348394f
jackyliu1999 <47201132+jackyliu1999@users.noreply.github.com>
2020-11-23 21:01:19 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "sdffs#@$%#RFDFSs",
+    password: "password",
     database: "myapp"
 });
 
